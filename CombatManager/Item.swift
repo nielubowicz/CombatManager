e898d5767d0d089583276d865da0934a0b328552
Chris Nielubowicz <nielubowicz@gmail.com>
2025-08-13 09:42:59 -0400

    Initial Commit

---
+++ CombatManager/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CombatManager
+//
+//  Created by mac on 8/13/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ CombatManagerTests/CombatManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CombatManagerTests.swift
+//  CombatManagerTests
+//
+//  Created by mac on 8/13/25.
+//
+
+import Testing
+@testable import CombatManager
+
+struct CombatManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
